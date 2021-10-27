a6e3ddd6827b181ee3ec600f13f4d29e59df969e
Kai <ali.rf.kashani@gmail.com>
2021-10-27 16:57:38 -0400

    creating directory for redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+// 	method: 'GET',
+// 	url: 'https://coinranking1.p.rapidapi.com/stats',
+// 	headers: {
+// 		'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+// 		'x-rapidapi-key': 'c7f5d6f6d3msh86f7d73267f03d5p11103cjsn6254e0bda780',
+// 	},
+// }
